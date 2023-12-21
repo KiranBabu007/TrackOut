ca5d70a2e42fd63f7da88088275e3160cd5e2a43
Kiran Babu <kiranbabu3355@gmail.com>
2023-12-21 20:27:03 +0530

    Mongo DB Connected

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express=require('express')
-const workoutroute=require('./routes/workout')
+const express = require("express");
+const workoutroute = require("./routes/workout");
+const mongoose = require("mongoose");
+const app = express();
 
-const app=express()
+app.use(express.json());
 
-app.use((req,res)=>{
-    console.log(req.path,req.method)
-    next( )
-})
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-app.use('/api/workouts',workoutroute)
+app.use("/api/workouts", workoutroute);
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
+}).catch((err)=>{
+ console.log(err)
+});
 
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Listening on port ${process.env.PORT}`)
-})

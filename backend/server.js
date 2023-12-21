5f034c165619449ff7f0459c565bd23812e6f133
Kiran Babu <kiranbabu3355@gmail.com>
2023-12-21 19:51:16 +0530

    Routes added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 require('dotenv').config()
 
 const express=require('express')
+const workoutroute=require('./routes/workout')
 
 const app=express()
 
-app.get('/',(req,res)=>{
-    res.json({msg:"Welcome to the app"})
+app.use((req,res)=>{
+    console.log(req.path,req.method)
+    next( )
 })
+
+app.use('/api/workouts',workoutroute)
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`Listening on port ${process.env.PORT}`)

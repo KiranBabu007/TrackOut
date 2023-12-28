9d0231653a4af62bc23acd1e9aecf59f7fbca494
Kiran Babu <kiranbabu3355@gmail.com>
2023-12-29 01:30:18 +0530

    Form components created

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,17 +1,37 @@
-import { useState } from "react"
+import { useState } from "react";
 
-impor
+impor;
 
-const WorkoutForm=()=>{
-    const [title,setTitle] = useState('')
-    const [load,setLoad] = useState('')
-    const [reps,setReps] = useState('')
-    
-    return(
-       <form className="create">
-        <h3>Add a New Workout</h3>
-        <label>Exercise Title</label>
-        <input type="text" />
-       </form>
-    )
-}
+const WorkoutForm = () => {
+  const [title, setTitle] = useState("");
+  const [load, setLoad] = useState("");
+  const [reps, setReps] = useState("");
+
+  return (
+    <form className="create">
+      <h3>Add a New Workout</h3>
+      <label>Exercise Title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+
+      <label>Load (in kg):</label>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+
+      <label>Reps:</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+
+      <button>Add Workout</button>
+    </form>
+  );
+};

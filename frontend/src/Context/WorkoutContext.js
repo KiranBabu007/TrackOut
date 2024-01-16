acaccd3c5165fde99fcc030a062d7844e2d56e34
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-16 16:28:26 +0530

    Refactor WorkoutContext.js

--- frontend/src/Context/WorkoutContext.js
+++ frontend/src/Context/WorkoutContext.js
@@ -1,25 +1,33 @@
-import { createContext,useReducer } from "react";
+import { createContext, useReducer } from "react";
 
 export const WorkoutContext = createContext();
 
 export const workoutsReducer = (state, action) => {
-    switch(action.type){
-        case 'SET_WORKOUTS':
-            
-    }
-}
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+  }
+};
 
-export const WorkoutContextProvider =({children})=>{
-   
-    const [state,dispatch] = useReducer(workoutsReducer,{
-        workouts:null
-    })
-    
-    
+export const WorkoutContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: null,
+  });
 
-    return(
-        <WorkoutContext.Provider value={}>
-            {children}
-        </WorkoutContext.Provider>
-    )
-}
+  return (
+    <WorkoutContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </WorkoutContext.Provider>
+  );
+};

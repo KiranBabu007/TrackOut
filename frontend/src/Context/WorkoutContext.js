005aba3a89a6b66cf9e7348ccf6369f3e475f9f9
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-16 16:20:29 +0530

    Add workoutsReducer to WorkoutContext.js

--- frontend/src/Context/WorkoutContext.js
+++ frontend/src/Context/WorkoutContext.js
@@ -1,10 +1,22 @@
-import { createContext } from "react";
+import { createContext,useReducer } from "react";
 
 export const WorkoutContext = createContext();
 
-export const WorkoutContextProvider =({children})=>{
+export const workoutsReducer = (state, action) => {
+    switch(action.type){
+        case 'SET_WORKOUTS':
+            
+    }
+}
 
+export const WorkoutContextProvider =({children})=>{
+   
+    const [state,dispatch] = useReducer(workoutsReducer,{
+        workouts:null
+    })
     
+    
+
     return(
         <WorkoutContext.Provider value={}>
             {children}

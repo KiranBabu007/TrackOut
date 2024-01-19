4000a9f43133be6cac7e707f5a61fc1a2ddc3786
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-19 23:45:19 +0530

    Add delete functionality to WorkoutDetails component

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,22 @@
 
 
 const WorkoutDetails = ({workout}) => {
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/'+workout._id,{
+      method: 'DELETE'
+    })
+    const json = await response.json()
+    
+  }
+
   return (
     <div className="workout-details">
         <h4>{workout.title}</h4>
         <p><strong>Load(kg):{workout.load}</strong></p>
         <p><strong>Reps:{workout.reps}</strong></p>
         <p>{workout.createdAt}</p>
+        <span onClick={handleClick}>delete</span>
     </div>
   )
 }

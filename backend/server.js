70533ca2cf727b0a76613c2010056718f9daa72a
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-20 10:45:05 +0530

    Add cors package and enable CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const express = require("express");
 const workoutroute = require("./routes/workout");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -14,12 +16,13 @@ app.use((req, res, next) => {
 
 app.use("/api/workouts", workoutroute);
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-app.listen(process.env.PORT, () => {
-  console.log(`Connected to db and Listening on port ${process.env.PORT}`);
-});
-}).catch((err)=>{
- console.log(err)
-});
-
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to db and Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });

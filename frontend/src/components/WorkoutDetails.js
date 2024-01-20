17b39ef7aa723383c7523d488c4b0509ee6af97e
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-21 01:38:39 +0530

    m

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,31 +1,39 @@
-import { useWorkoutsContext } from "../hooks/WorkoutContext"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { useWorkoutsContext } from "../hooks/WorkoutContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-const WorkoutDetails = ({workout}) => {
-
-  const { dispatch } = useWorkoutsContext()
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
 
   const handleClick = async () => {
-    const response = await fetch('/api/workouts/'+workout._id,{
-      method: 'DELETE'
-    })
-    const json = await response.json()
+    const response = await fetch(
+      "https://track-out.onrender.com/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await response.json();
 
-    if(response.ok){
-      dispatch({type:'DELETE_WORKOUT', payload: json})
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
     }
-  }
+  };
 
   return (
     <div className="workout-details">
-        <h4>{workout.title}</h4>
-        <p><strong>Load(kg):{workout.load}</strong></p>
-        <p><strong>Reps:{workout.reps}</strong></p>
-        <p>{workout.createdAt}</p>
-        <span onClick={handleClick}><FontAwesomeIcon icon={faTrashAlt} /></span>
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load(kg):{workout.load}</strong>
+      </p>
+      <p>
+        <strong>Reps:{workout.reps}</strong>
+      </p>
+      <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>
+        <FontAwesomeIcon icon={faTrashAlt} />
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default WorkoutDetails
+export default WorkoutDetails;

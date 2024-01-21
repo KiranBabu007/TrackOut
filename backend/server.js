4c9a4b78d00817260787bc4a3906500982d06da5
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-21 18:58:57 +0530

    Add server ping functionality

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,32 @@ const workoutroute = require("./routes/workouts");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const https = require('https');
+
+exports.handler = async (event, context) => {
+ const url = 'https://track-out.vercel.app/';
+
+ return new Promise((resolve, reject) => {
+   const req = https.get(url, (res) => {
+     if (res.statusCode === 200) {
+       resolve({
+         statusCode: 200,
+         body: 'Server pinged successfully',
+       });
+     } else {
+       reject(
+         new Error(`Server ping failed with status code: ${res.statusCode}`)
+       );
+     }
+   });
+
+   req.on('error', (error) => {
+     reject(error);
+   });
+
+   req.end();
+ });
+};
 
 app.use(express.json());
 

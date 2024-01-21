3d3a9aad8c1bc1d4d5a2e26bbbd7312c1ffcdcb5
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-21 13:40:08 +0530

    Update CORS origin in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,31 +7,15 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
+
 app.use(
   cors({
-    origin: "https://track-out.vercel.app/",
+    origin: "https://track-out.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204,
   })
 );
-
-app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://track-out.vercel.app/");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(204);
-  } else {
-    next();
-  }
-});
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);

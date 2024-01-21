aa812d6837cbf884cba36200ffeb737344616cec
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-21 13:10:29 +0530

    Update API endpoint URL in home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -9,7 +9,7 @@ const Home = () => {
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch(
-        "/api/workouts"
+        "https://track-out.onrender.com/api/workouts"
       );
       const json = await response.json();
 

ec5853f12627c7856774c872711a0b9230c98981
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-22 19:42:07 +0530

    Fix API endpoint URLs

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -14,7 +14,7 @@ const WorkoutForm = () => {
     e.preventDefault();
 
     const workout = { title, load, reps };
-    const response = await fetch(" https://3912-103-182-167-52.ngrok-free.app/api/workouts", {
+    const response = await fetch(" https://3912-103-182-167-52.ngrok-free.app/api/workouts/", {
       method: "POST",
       body: JSON.stringify(workout),
       headers: {
